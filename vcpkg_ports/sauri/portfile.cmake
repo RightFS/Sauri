b81225b9ab0e7d8c930b3f5a7a4ad4d60ba39c6c
Octopus.R <Right.Sun@outlook.com>
2025-05-29 13:46:18 +0800

    fix vcpkg port

--- vcpkg_ports/sauri/portfile.cmake
+++ vcpkg_ports/sauri/portfile.cmake
@@ -5,21 +5,26 @@
 
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
-        REPO Tencent/sqlcipher
-        REF 5d8825c22feedb421ad6f9ecfc1399460e10d299
-        HEAD_REF wcdb
-        SHA512 f08017e9b0b5405b953ebfaac00a5524cb4fc9e3f3f2cfb21dd988290c20713d97336bf2ddd2ff3f861256cb230a9a3fb9eef441ffd0b410b1c40613371dbf2a
-        PATCHES
-        patches/fix-sqlcipher.patch
+        REPO RightFS/sauri
+        REF 8b1c138ed40f2bc8cedb983598e22a0a8e83e036
+        HEAD_REF master
+        SHA512 ac747cfe2d0c517468b55112355720d0a93f4a19977466fbdacd2c85060b61d60fc7aa87606400b7f4d45eb734f893135285e4a3a14d0e0b422ad89682a23f54
 )
 
-# 检查是否存在 sqlcipher.cmake（需根据实际路径验证）
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+        -DENABLE_EXAMPLES=OFF
+        -DENABLE_TESTS=OFF
 )
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME "sqlcipher")
 
-file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH share/sauri)
+vcpkg_copy_pdbs()
+
+# Remove debug include directories - THIS FIXES YOUR ERROR
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+#file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+#vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
